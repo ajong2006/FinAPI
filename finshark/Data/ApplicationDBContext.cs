8272882b848ed5ff866df71db2233e02a9479217
ajong2006 <ekokobe.ajong@gmail.com>
2024-02-19 15:48:26 -0600

    Fixed duplicate function

--- finshark/Data/ApplicationDBContext.cs
+++ finshark/Data/ApplicationDBContext.cs
@@ -49,38 +49,6 @@ namespace finshark.Data
             builder.Entity<IdentityRole>().HasData(roles);
         }
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-            builder.Entity<Portfolio>(x => x.HasKey(p => new {p.AppUserId, p.StockId}));
-
-            builder.Entity<Portfolio>()
-                .HasOne(u => u.AppUser)
-                .WithMany(u => u.Portfolios)
-                .HasForeignKey(u => u.AppUserId);
-
-            builder.Entity<Portfolio>()
-                .HasOne(u => u.Stock)
-                .WithMany(u => u.Portfolios)
-                .HasForeignKey(u => u.StockId);
-
-
-            List<IdentityRole> roles = new List<IdentityRole>
-            {
-                new IdentityRole
-                {
-                    Name = "Admin",
-                    NormalizedName = "ADMIN"
-                },
-                new IdentityRole
-                {
-                    Name = "User",
-                    NormalizedName = "USER"
-                }
-            };
-            builder.Entity<IdentityRole>().HasData(roles);
-        }
 
         /* Dont forget to run migration after making this file */
     }
