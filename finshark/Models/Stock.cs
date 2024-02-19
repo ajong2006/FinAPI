9d2986678b848a3996ff0c4c7c464da65f4a1c57
ajong2006 <ekokobe.ajong@gmail.com>
2024-02-19 15:29:56 -0600

    Added app user & portfolio functionality

--- finshark/Models/Stock.cs
+++ finshark/Models/Stock.cs
@@ -2,6 +2,7 @@
 
 namespace finshark.Models
 {
+    [Table("Stocks")]
     public class Stock
     {
         public int Id { get; set; }
@@ -16,6 +17,7 @@ namespace finshark.Models
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
+        public List<Portfolio> Portfolios { get; set; } = new List<Portfolio>();
 
     }
 }
